9e377790b7391fe153750a1821b4ecc71ef72601
Unai Elordi Hidalgo <uelordi@vicomtech.org>
2017-05-22 17:29:54 +0200

    adding logger

---
+++ CallRenderScriptFromNDK/app/src/main/cpp/logger.h
@@ -0,0 +1,17 @@
+#ifndef LOGGER_H
+#define LOGGER_H
+
+
+#include <strings.h>
+#include <android/log.h>
+
+#define LOG_INFO(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+#define LOG_ERROR(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+
+#define LOGV(...) __android_log_print(ANDROID_LOG_VERBOSE, LOG_TAG,__VA_ARGS__)
+#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG  , LOG_TAG,__VA_ARGS__)
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO   , LOG_TAG,__VA_ARGS__)
+#define LOGW(...) __android_log_print(ANDROID_LOG_WARN   , LOG_TAG,__VA_ARGS__)
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR  , LOG_TAG,__VA_ARGS__)
+
+#endif
